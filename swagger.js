289485c7718341466053e1296d1b7d308cfa3d08
Davi Bueno <davi_bueno1998@hotmail.com>
2024-09-05 14:11:08 -0300

    Remove node_modules do repositório

---
+++ swagger.js
@@ -0,0 +1,35 @@
+const swaggerJsdoc = require('swagger-jsdoc');//biblio para gerar a  doc
+const swaggerUi = require('swagger-ui-express');//interface do uso da doc
+
+const swaggerOptions = {//configs
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Venda Eletrodomésticos',
+      version: '1.0.0',
+      description: 'API destinado à uso de CRUDS',
+    },
+    components: {
+      securitySchemes: {//config de segurança
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ['./API-vendas/route/*.js'], // de onde ele vai puxar as ditas docs(nos routes)
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);//uso das configs
+
+module.exports = {
+  swaggerUi,
+  swaggerDocs,
+};
